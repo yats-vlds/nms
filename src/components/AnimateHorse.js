8a1cdbcca8e9642e4289c6605f774f95601c2813
Vladislav <getelementbyidkango@gmail.com>
2021-02-23 10:21:03 +0200

    before

--- src/components/AnimateHorse.js
+++ src/components/AnimateHorse.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "./AnimateHorse.css"
-import shakhamatyTable from "../assets/шахматы (1).svg"
+import shakhamatyTable from "../assets/chess.svg"
 import kon from "../assets/chessHorse.svg"
 import kind from "../assets/chessKind.svg"
 
