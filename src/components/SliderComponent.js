79b6aa8942713a451c331906c8abbb733d43b114
Vladislav <getelementbyidkango@gmail.com>
2021-03-04 08:44:12 +0200

    add text\

--- src/components/SliderComponent.js
+++ src/components/SliderComponent.js
@@ -1,15 +1,8 @@
 import React from "react";
-import yog from "../assets/yog.svg"
 import shadow from "../assets/shadow.png";
 import table1pryamogolnik from "../assets/table1Pryamogolnik.svg";
 import sloygoPrint from "../assets/sloygoPrint.svg";
 import goPrintOriginal from "../assets/goPrintOriginal.svg";
-import shadow2 from "../assets/shadowTwo.png";
-import tableTwoTwo from "../assets/tableTwoTwo.svg";
-import inmore from "../assets/inmore.svg";
-import tableThree1 from "../assets/tableThree1.png";
-import tableTree2 from "../assets/tableThree2.svg";
-import tableThree4 from "../assets/tableThree4.svg";
 import Slider from "react-slick";
 
 const SliderComponent = () => {
@@ -60,20 +53,27 @@ const SliderComponent = () => {
                                     SaaS Development Services
                                 </h1>
                                 <h1 className="bootstrap__tableTitle bootstrap__marginTop40">
-                                    Project name
+                                    GoPrint
                                 </h1>
-                                <p className="bootstrap__tableText marginBottom">No matter what type of your
-                                    work you do, NMS customized
-                                    software development will fulfill your needs. No matter
-                                    what type of your work you do, NMS customized software
-                                    development will fulfill your needstjxfyk uirtftyityerytyt.
-                                    software development will fulfill your needs. No matter
-                                    what type of your work you do, NMS customized software
-                                    development will fulfill your needstjxfyk uirtftyityerytyt.</p>
-                                <h4 className="bootstrap__tableTwoTitle">
-                                    Design, UX, UI, Java <br/>
-                                    Web, Logo, Brend
-                                </h4>
+                                <h3 className="bootstrap__tableTwoTitle">
+                                    Idea
+                                </h3>
+                                <p className="bootstrap__tableText marginBottom">
+                                    What about automation of printing services? We made it self-service. NMS Team took
+                                    responsibility for the full scale of works including UI/UX design, web SaaS,
+                                    cross-platform custom mobile applications development, Backend development, Admin
+                                    Dashboard creation and unique programming solution for the innovative remote
+                                    printing terminals.</p>
+                                <h3 className="bootstrap__tableTwoTitle">
+                                    Tech Picture
+                                </h3>
+                                <p>
+                                    React/React Native<br/>
+                                    Next.js<br/>
+                                    Node.js (Express)<br/>
+                                    Postgres<br/>
+                                    Docker
+                                </p>
                             </div>
                         </div>
                     </div>
@@ -90,10 +90,10 @@ const SliderComponent = () => {
                             <div className="bootstrap__tableLineOne"></div>
                             <Slider {...settings} className="slider-subslider">
                                 <div>
-                                    <img src={shadow} className="tableOneIcon1"/>
-                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
-                                    <img src={sloygoPrint} className="tableOneIcon3" loading="lazy"/>
-                                    <img src={goPrintOriginal} className="tableOneIcon4" loading="lazy"/>
+                                    {/*<img src={shadow} className="tableOneIcon1"/>*/}
+                                    {/*<img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>*/}
+                                    {/*<img src={sloygoPrint} className="tableOneIcon3" loading="lazy"/>*/}
+                                    {/*<img src={goPrintOriginal} className="tableOneIcon4" loading="lazy"/>*/}
                                 </div>
                                 <div>
                                 </div>
@@ -110,20 +110,28 @@ const SliderComponent = () => {
                                     SaaS Development Services
                                 </h1>
                                 <h1 className="bootstrap__tableTitle bootstrap__marginTop40">
-                                    Project name
+                                    Listery
                                 </h1>
-                                <p className="bootstrap__tableText marginBottom">No matter what type of your
-                                    work you do, NMS customized
-                                    software development will fulfill your needs. No matter
-                                    what type of your work you do, NMS customized software
-                                    development will fulfill your needstjxfyk uirtftyityerytyt.
-                                    software development will fulfill your needs. No matter
-                                    what type of your work you do, NMS customized software
-                                    development will fulfill your needstjxfyk uirtftyityerytyt.</p>
-                                <h4 className="bootstrap__tableTwoTitle">
-                                    Design, UX, UI, Java <br/>
-                                    Web, Logo, Brend
-                                </h4>
+                                <h3 className="bootstrap__tableTwoTitle">
+                                    Idea
+                                </h3>
+                                <p className="bootstrap__tableText marginBottom">If you take care about your friends and
+                                    want to make a gift search process easier for them, share your desires with Listery.
+                                    This MVP app allows you to search for desired items via databases of the biggest
+                                    web-shops and to share your lists. Our dev team had a pleasure to arrange the app’s
+                                    back end part.<br/>
+                                    Who knows, maybe with this Flutter SDK based product your innermost desires will
+                                    finally come true!
+                                </p>
+                                <h3 className="bootstrap__tableTwoTitle">
+                                    Tech Picture
+                                </h3>
+                                <p>
+                                    Flutter<br/>
+                                    Node.js (Express)<br/>
+                                    Mongo<br/>
+                                    Docker
+                                </p>
                             </div>
                         </div>
                     </div>
