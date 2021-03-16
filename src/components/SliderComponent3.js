f0f1ad5445dbaccb8455b2c6134bd5c34951ea81
Vladislav <getelementbyidkango@gmail.com>
2021-03-16 16:03:44 +0200

    until the changes

--- src/components/SliderComponent3.js
+++ src/components/SliderComponent3.js
@@ -4,6 +4,19 @@ import tableTree2 from "../assets/tableThree2.svg";
 import tableThree4 from "../assets/tableThree4.svg";
 import Slider from "react-slick";
 import LazyLoad from "react-lazy-load"
+import UkrTelecom1 from "../assets/UKT-900x1025-01.png"
+import UkrTelecom2 from "../assets/UKT-900x1025-02.png"
+import UkrTelecom3 from "../assets/UKT-900x1025-03.png"
+import GoPrintDesign1 from "../assets/DIZ-900x1025-01.png"
+import GoPrintDesign2 from "../assets/DIZ-900x1025-02.png"
+import GoPrintDesign3 from "../assets/DIZ-900x1025-03.png"
+import GoPrintDesign4 from "../assets/DIZ-900x1025-04.png"
+import InMoreDesign1 from "../assets/InMoreDIZ-900x1025-01.png"
+import InMoreDesign2 from "../assets/InMoreDIZ-900x1025-02.png"
+import InMoreDesign3 from "../assets/InMoreDIZ-900x1025-03.png"
+import shadow from "../assets/shadow.png";
+import table1pryamogolnik from "../assets/table1Pryamogolnik.svg";
+
 
 const SliderComponent = () => {
     const settings = {
@@ -33,12 +46,24 @@ const SliderComponent = () => {
                             <div className="bootstrap__tableLineThree"></div>
                             <Slider {...settings} className="slider-subslider">
                                 <div>
-                                    {/*<img src={tableThree1} loading="lazy" className="tableThreeIcon1"/>*/}
-                                    {/*<img src={tableTree2} loading="lazy" className="tableThreeIcon2"/>*/}
-                                    {/*<img src={tableThree4} loading="lazy" className="tableThreeIcon3"/>*/}
-                                    {/*/!*<img src={tableThree3} className="icon3TableThree"/>*!/*/}
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={GoPrintDesign1} className="tableOneIcon4" loading="lazy"/>
                                 </div>
                                 <div>
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={GoPrintDesign2} className="tableOneIcon4" loading="lazy"/>
+                                </div>
+                                <div>
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={GoPrintDesign3} className="tableOneIcon4" loading="lazy"/>
+                                </div>
+                                <div>
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={GoPrintDesign4} className="tableOneIcon4" loading="lazy"/>
                                 </div>
                             </Slider>
                         </div>
@@ -76,6 +101,67 @@ const SliderComponent = () => {
                         </div>
                     </div>
                 </div>
+               <div className="carousel-item">
+                   <div className="bootstrap__table bootstrap__tableThree">
+                       <div className="bootstrap__tableLeft">
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <Slider {...settings} className="slider-subslider">
+                               <div>
+                                   <img src={shadow} className="tableOneIcon1"/>
+                                   <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                   <img src={InMoreDesign1} className="tableOneIcon4" loading="lazy"/>
+                               </div>
+                               <div>
+                                   <img src={shadow} className="tableOneIcon1"/>
+                                   <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                   <img src={InMoreDesign2} className="tableOneIcon4" loading="lazy"/>
+                               </div>
+                               <div>
+                                   <img src={shadow} className="tableOneIcon1"/>
+                                   <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                   <img src={InMoreDesign3} className="tableOneIcon4" loading="lazy"/>
+                               </div>
+                           </Slider>
+                       </div>
+                       <div className="bootstrap__tableRight">
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableLineThree"></div>
+                           <div className="bootstrap__tableRightInfo">
+                               <h1 className="bootstrap__tableTitle bootstrap_tableRightContentTitle">
+                                   UI/UX Design Services
+                               </h1>
+                               <h1 className="bootstrap__tableTitle">
+                                   In&More
+                               </h1>
+                               <h3 className="bootstrap__tableTwoTitle">
+                                   Challenge
+                               </h3>
+                               <p className="bootstrap__tableText marginBottom">
+                                   Creation of the website for the interior design bureau placed in Berlin. The product
+                                   is Word Press based so it definitely needed something stylish and fresh to look
+                                   great! Simple and easy – that’s what perfectly suits successful businesses!
+                               </p>
+                               <h3 className="bootstrap__tableTwoTitle">
+                                   Instruments
+                               </h3>
+                               <p>
+                                   Adobe Photoshop<br/>
+                                   Illustrator<br/>
+                                   Webflow
+                               </p>
+                           </div>
+                       </div>
+                   </div>
+               </div>
                 <div className="carousel-item">
                     <div className="bootstrap__table bootstrap__tableThree">
                         <div className="bootstrap__tableLeft">
@@ -88,66 +174,19 @@ const SliderComponent = () => {
                             <div className="bootstrap__tableLineThree"></div>
                             <Slider {...settings} className="slider-subslider">
                                 <div>
-                                    {/*<img src={tableThree1} loading="lazy" className="tableThreeIcon1"/>*/}
-                                    {/*<img src={tableTree2} loading="lazy" className="tableThreeIcon2"/>*/}
-                                    {/*<img src={tableThree4} loading="lazy" className="tableThreeIcon3"/>*/}
-                                    {/*/!*<img src={tableThree3} className="icon3TableThree"/>*!/*/}
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={UkrTelecom1} className="tableOneIcon4" loading="lazy"/>
                                 </div>
                                 <div>
-                                </div>
-                            </Slider>
-                        </div>
-                        <div className="bootstrap__tableRight">
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableRightInfo">
-                                <h1 className="bootstrap__tableTitle bootstrap_tableRightContentTitle">
-                                    UI/UX Design Services
-                                </h1>
-                                <h1 className="bootstrap__tableTitle">
-                                    In&More
-                                </h1>
-                                <h3 className="bootstrap__tableTwoTitle">
-                                    Challenge
-                                </h3>
-                                <p className="bootstrap__tableText marginBottom">
-                                    Creation of the website for the interior design bureau placed in Berlin. The product
-                                    is Word Press based so it definitely needed something stylish and fresh to look
-                                    great! Simple and easy – that’s what perfectly suits successful businesses!
-                                </p>
-                                <h3 className="bootstrap__tableTwoTitle">
-                                    Instruments
-                                </h3>
-                                <p>
-                                    Adobe Photoshop<br/>
-                                    Illustrator<br/>
-                                    Webflow
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="carousel-item">
-                    <div className="bootstrap__table bootstrap__tableThree">
-                        <div className="bootstrap__tableLeft">
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <div className="bootstrap__tableLineThree"></div>
-                            <Slider {...settings} className="slider-subslider">
-                                <div>
-                                    {/*<img src={tableThree1} loading="lazy" className="tableThreeIcon1"/>*/}
-                                    {/*<img src={tableTree2} loading="lazy" className="tableThreeIcon2"/>*/}
-                                    {/*<img src={tableThree4} loading="lazy" className="tableThreeIcon3"/>*/}
-                                    {/*/!*<img src={tableThree3} className="icon3TableThree"/>*!/*/}
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={UkrTelecom2} className="tableOneIcon4" loading="lazy"/>
                                 </div>
                                 <div>
+                                    <img src={shadow} className="tableOneIcon1"/>
+                                    <img src={table1pryamogolnik} className="tableOneIcon2" loading="lazy"/>
+                                    <img src={UkrTelecom3} className="tableOneIcon4" loading="lazy"/>
                                 </div>
                             </Slider>
                         </div>
